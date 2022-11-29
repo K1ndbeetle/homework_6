eaf217ed1fc1812b16b6e14a2cfed3aaa3514c00
K1ndbeetle <alx-s@bk.ru>
2022-11-29 20:57:23 +0300

    Homework2

--- src/Main.java
+++ src/Main.java
@@ -38,5 +38,55 @@ public class Main {
             populationCountryY += populationCountryY * growthPopulation / 1000;
             System.out.println("Год " + populationYear + ", численность населения составляет " + populationCountryY + " человек");
         }
+        System.out.println();
+
+        //homework2
+        //task №1
+        System.out.println("Задача №1");
+        int deposit = 15_000;
+        double monthlyPercentage = 0.07;
+        int total = 12_000_000;
+        int month = 1;
+        for (; deposit < total; month++) {
+            deposit += deposit*monthlyPercentage;
+            System.out.println("Месяц " + month + ", сумма накоплений равна " + deposit + " рублей");
+        }
+        System.out.println();
+
+        //task №2
+        System.out.println("Задача №2");
+        int contribution = 15_000;
+        double monthPercent = 0.07;
+        int full = 12_000_000;
+        int m = 1;
+        for (; contribution < full; m++) {
+            contribution += contribution * monthPercent;
+            if (m % 6 == 0) {
+                System.out.println("Месяц " + m + ", сумма накоплений равна " + contribution + " рублей");
+            }
+        }
+        System.out.println();
+
+        //task №3
+        System.out.println("Задача №3");
+        int initialAmount = 15_000;
+        double percent = 0.07;
+        int month1 = 1;
+        for (; month1 <= 9*12; month1++) {
+            initialAmount += initialAmount * percent;
+            if (month1 % 6 == 0) {
+                System.out.println("Месяц " + month1 + ", сумма накоплений равна " + initialAmount + " рублей");
+            }
+        }
+        System.out.println();
+
+        //task №4
+        System.out.println("Задача №4");
+        int friday = 5;
+        int daysMonth = 31;
+        for (; friday < daysMonth; friday += 7) {
+            System.out.println("Сегодня пятница, " + friday + "-е число. Необходимо подготовить отчет.");
+        }
+        System.out.println();
     }
 }
